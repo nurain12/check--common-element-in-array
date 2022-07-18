c7c1e8cbba086b001d559400d607a651d6ec0e91
nurain12 <75721935+nurain12@users.noreply.github.com>
2022-07-18 16:09:10 +0800

    Try Upload
    Try jerr

---
+++ CommonElementInArray/CommonElementInArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CommonElementInArray
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayA = { 1, 2, 3, 4, 5 };
+            int[] arrayB = { 3, 2, 9, 3, 7 };
+
+            for (int i = 0; i < arrayA.Length; i++)
+            {
+                for(int j = 0; j < arrayB.Length; j++) 
+                { 
+                    if(arrayA[i] == arrayB[j])
+                    {
+                        Console.WriteLine(arrayA[i]);
+                    }
+                }
+            }
+        }
+     }
+}
+
